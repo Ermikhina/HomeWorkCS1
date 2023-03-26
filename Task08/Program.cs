6b601e6c5bf21b663be05f6fc1df316f2b2b00d8
Natal <nat_n_e@mail.ru>
2023-03-26 15:10:57 +0300

    Program fot Task8

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число
+//  (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Вариант 1
+
+// Console.Write("Input number N > 2: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int count = 2;
+// while (count <= number)
+// {
+// Console.Write($"{count}  ");
+// count = count + 2;
+// }
+// Вариант 2
+
+Console.Write("Input number N > 2: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= number)
+{
+    if (count % 2 == 0)
+    Console.Write($"{count}  ");
+    count ++;
+}
